3b31e627850d416420886cc28f8998ef88f02737
lozunoff <andrey.lozunov@gmail.com>
2020-01-25 13:26:07 +0400

    Базовые файлы почищены от кода стандартного примера

--- src/index.js
+++ src/index.js
@@ -1,58 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron'); // eslint-disable-line
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// Обработка создания/удаления ярлыков при установке/удалении
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Переменная для хранения глобальной ссылки на объект окна
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window.
+  // Создаём окно браузера
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
-  // and load the index.html of the app.
+  // Загружаем UI
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
+  // Отображаем средства разработчика
   mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
+  // Обрабатываем событие закрытия окна
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Приступаем к созданию окна после инициализации Electron
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
+// Выходим, когда все окна будут закрыты.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// Пересоздаем окно на MacOS после того, как на иконку в доке нажали и других открытых окон нет
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
